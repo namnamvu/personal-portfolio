52ca7d8d8b550c310da0c6613b64199da37c6c27
Nam Vũ <namvu@Nams-MacBook-Air.local>
2025-06-19 03:58:12 -0400

    Change to PORT and change import dotenv config

--- server/server.js
+++ server/server.js
@@ -1,20 +1,24 @@
+import 'dotenv/config';
+
 import express from 'express';
 import helmet from 'helmet'
 import morgan from 'morgan';
 import cors from 'cors';
-import dotenv from 'dotenv'
 import contactRoutes from './routes/contactRoutes.js'
 
-// Allow PORT to be used fron .env
-dotenv.config();
 
 // Create an instance of express app
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
+
 // Allow frontend to interact with backend without getting block by cors
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000', // Your Vite frontend
+    methods: ['GET', 'POST', 'OPTIONS'],
+    credentials: false
+  }));
 
 // Security amiddleware to protect app by setting HTTP headers
 app.use(helmet());
@@ -22,6 +26,11 @@ app.use(helmet());
 app.use(morgan("dev"));
 
 app.use('/api/contact', contactRoutes)
+
+// Add a test root route
+app.get('/', (req, res) => {
+    res.send('Server is running');
+  });  
 
 app.listen(PORT, () => {
     console.log("Server is runnning on port " + PORT)
