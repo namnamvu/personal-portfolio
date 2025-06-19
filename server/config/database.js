10e302be1bf8038155eae1e503708cd0b9c6654b
Nam Vũ <namvu@Nams-MacBook-Air.local>
2025-06-19 03:59:00 -0400

    Add some log function to database.js

--- server/config/database.js
+++ server/config/database.js
@@ -1,6 +1,6 @@
 // server/config/database.js
 import pkg from 'pg';
-import { AppError } from '../utils/appError.js';
+import { AppError } from '../utils/AppError.js';
 
 const { Pool } = pkg;
 // Create our pool for data operation
@@ -8,8 +8,15 @@ const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
+    password: String(process.env.DB_PASSWORD),
     port: process.env.DB_PORT || 5432
+  });
+  // Logging to check if it exist
+  console.log({
+    DB_USER: process.env.DB_USER,
+    DB_HOST: process.env.DB_HOST,
+    DB_NAME: process.env.DB_NAME,
+    DB_PASSWORD: process.env.DB_PASSWORD,
   });
   // Check for success
   pool.on('connect', () => console.log('Connected to PostgreSQL'));
